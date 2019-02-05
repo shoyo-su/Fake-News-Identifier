983f65c552eaff8ac255987d416103195d507629
Ajit Bhalerao <ajitbhalerao74@gmail.com>
2019-02-05 23:46:40 +0530

    Update crawler.py
    Updated with a search option but still has issues with byte literal to str conversion

--- crawler.py
+++ crawler.py
@@ -24,16 +24,44 @@ url_list=url_input()
 
 content_l=html_extractor(url_list)
 
-print(str(content_l))
+print(str(content_l[:300]))
 
 
-'''
+
 def word_finder(content_l):
     word=input("Search word:")
-    content=" ".join(content_l)
-    return content.find(word)
+    word=str(word)
+    #content=" ".join(content_l)
+    pos_l=[]
+    content_l=content_l[0]
+    content_len=len(content_l)
+    nl=[]
+    ide=0
+    while ide<content_len:
+        wr=content_l[ide]
+        nl.append(wr.decode('Latin-1'))
+        ide=ide+1
+    content_l="".join(nl)    
+    word_len=len(word)
+    content_len=len(content_l)
+    iter_index=0
+    while iter_index<content_len:
+    
+        check_word=content_l[iter_index:iter_index+word_len:word_len]
+        print(word_len,iter_index)
+        print(check_word)
+        #check_word=check_word.decode('Latin-1')
+        
+        print(check_word,word)
+        if check_word==word:
+            print("went in")
+            pos_l.append(iter_index)
+        iter_index=iter_index+word_len
+    return pos_l
+
+
 print(word_finder(content_l))
-'''             
+             
         
         
     
