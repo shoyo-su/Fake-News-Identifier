9df4cb831710c2f95e10e2d49659616f26749996
Ajit Bhalerao <ajitbhalerao74@gmail.com>
2019-02-12 22:54:26 +0530

    The basic model for statistical analysis

---
+++ base_mod.py
@@ -0,0 +1,124 @@
+Article_1=['Shefali yaha article paste karo']
+Article_2=[' same as above']
+Article_3=['Jagrati yaha article paste karien !!!!']
+
+Article_list=[Article_1,Article_2,Article_3]
+
+Deaths=0
+Injuries=0
+Lost=0
+Cost_to_state=0
+Adults=0
+Date_Time=0
+
+line=30
+
+dict_1={"death":5,"lost":4} #Yaha pe apne apne words ki word_length likh dena ':' laga ke
+
+for article in Article_list:
+    line=article[line]
+    word_len=dict_1["Your word"]
+
+    index=0
+    while index<30:
+        word=line[word_len]
+
+        if word=="lost":
+            lost=lost+1
+            
+        elif word=="":
+            lost=lost+1
+
+        #yaha niche baki elifs ayenge
+
+
+
+
+        index=index+1    
+
+            
+    index1=0
+    while index1<30:
+        word=line[word_len]
+
+        if word=="Death":
+            Deaths=-Deaths+1
+            
+        elif word=="":
+            Deaths=Deaths+1
+
+        index1=index+1
+        
+
+    index2=0
+    while index2<30:
+        word=line[word_len]
+
+        if word=="":
+            your_word=your_word+1
+            
+        elif word=="":
+            your_word=your_word+1
+
+        index2=index2+1
+        
+
+    index3=0
+    while index3<30:
+        word=line[word_len]
+
+        if word=="":
+            your_word=your_word+1
+            
+        elif word=="":
+            your_word=your_word+1
+
+
+
+
+
+
+        index3=index3+1
+
+
+        
+
+    index4=0
+    while index4<30:
+        word=line[word_len]
+
+        if word=="":
+            your_word=your_word+1
+            
+        elif word=="":
+            your_word=your_word+1
+
+
+        index4=index+1
+    
+
+    index5=0
+    while index5<30:
+        word=line[word_len]
+
+        if word=="":
+            your_word=your_word+1
+            
+        elif word=="":
+            your_word=your_word+1
+
+
+
+
+
+        index5=index5+1
+        
+
+            
+        
+    
+    
+
+
+
+
