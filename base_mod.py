ca6310b42101669c2a5a387292cec36af4fc6d66
shefalibadyal <46719810+shefalibadyal@users.noreply.github.com>
2019-02-17 17:34:59 +0530

    Update base_mod.py

--- base_mod.py
+++ base_mod.py
@@ -1,5 +1,122 @@
-Article_1=['Shefali yaha article paste karo']
-Article_2=[' same as above']
+Article_1=['On Sunday 26 December 2004, a magnitude 9 earthquake occurred off the West Coast of Northern Sumatra in the Indian Ocean. This caused the Indian Ocean tsunami that affected 13 countries and killed approximately 230,000 people.
+
+The Indian Ocean tsunami in December 2004 affected 13 countries. The epicentre was close to Indonesia
+This tsunami was particularly devastating because:
+
+The earthquake which caused the tsunami was magnitude 9.
+The epicentre was very close to some densely populated coastal communities, eg Indonesia. They had little or no warning. The only sign came just before the tsunami struck when the waterline suddenly retreated, exposing hundreds of metres of beach and seabed.
+There was no Indian Ocean tsunami warning system in place. This could have saved more people in other countries further away from the epicentre.
+Many of the countries surrounding the Indian Ocean are LEDCs so they could not afford to spend much on preparation and prevention.
+In some coastal areas, mangrove forests had been removed to make way for tourist developments and therefore there was less natural protection.
+Social impacts of the tsunami (effects on people)
+Hundreds of tents where people in Indonesia lived after houses were destroyed by the Indian Ocean tsunami
+230 000 deaths.
+1.7 million homeless.
+5-6 million needing emergency aid, eg food and water.
+Threat of disease from mixing of fresh water, sewage and salt water.
+1,500 villages destroyed in northern Sumatra.
+Economic impacts of the tsunami (effects on money and jobs)
+The commercial district of Banda Aceh, Indonesia reduced to rubble after the Indian Ocean tsunami
+Ports ruined.
+Fishing industry devastated - boats, nets and equipment destroyed. An estimated 60% of Sri Lanka’s fishing fleet destroyed.
+Reconstruction cost billions of dollars.
+Loss of earnings from tourism - foreign visitors to Phuket dropped 80% in 2005.
+Communications damaged, eg roads, bridges and rail networks.
+Environmental impacts of the tsunami
+Crops destroyed.
+Farm land ruined by salt water.
+8 million litres of oil escaped from oil plants in Indonesia.
+Mangrove forests along the coast were destroyed.
+Coral reefs and coastal wetlands damaged.
+Responses to the tsunami
+The army search the rubble for bodies after the tsunami in Banda Aceh, Indonesia
+Non-Governmental Organisations (NGOs) and local authorities typically have immediate and secondary responses to devastation of this kind.
+
+Immediate responses
+Search and rescue.
+Emergency food and water.
+Medical care.
+Temporary shelter.
+Re-establishing infrastructure and communications.
+Secondary responses
+Re-building and improving infrastructure and housing.
+Providing jobs and supporting small businesses.
+Giving advice and technical assistance.
+A man carries a basket of posessions through the mess left by the tsunami in Banda Aceh, Indonesia
+Responses to the 2004 Indian Ocean tsunami can also be divided into short and long term:
+
+Short-term responses
+In many areas local communities were cut off and had to help themselves.
+The authorities ordered quick burial or burning of the dead to avoid the spread of disease.
+Food aid was provided to millions of people, eg from the World Food Programme.
+$7 billion (just under £4.5billion) of aid was promised by foreign governments - but there were complaints that not all money pledged was given.
+The British public gave £330 million through charities, eg the average Actionaid donation was £84 - their best ever response.
+Long-term responses
+Reconstruction is still taking place.
+International scale: an Indian Ocean tsunami warning system has now been set up.
+Local scale: some small-scale sustainable development projects have been set up by charities to aid recovery and help local people help themselves to rebuild and set up small businesses.']
+Article_2=['MADRAS, India, Monday, Dec. 27 - The world's most powerful earthquake in 40 years erupted underwater off the Indonesian island of Sumatra on Sunday and sent walls of water barreling thousands of miles, killing more than 13,000 people in half a dozen countries across South and Southeast Asia, with thousands more missing or unreachable.
+
+The earthquake, which measured 9.0 in magnitude, set off tsunamis that built up speeds of as much as 500 miles per hour, then crashed into coastal areas of Sri Lanka, India, Thailand, Indonesia, the Maldives and Malaysia as 40-foot-high walls of water, devouring everything and everyone in their paths.
+
+Its force was felt more than 3,000 miles away in Somalia on the eastern coast of Africa, where nine people were reported killed.
+
+Aid agencies were rushing staff and equipment to the region, warning that rotting bodies were threatening health and water supplies.
+
+It took several hours in some cases on Sunday for the waves to build and reach their targets after the earthquake struck. But none of the most affected countries had warning systems in place to detect the coming onslaught and alert their citizens to move away from the coastline.
+
+Continue reading the main story
+ADVERTISEMENT
+
+Continue reading the main story
+
+"I just couldn't believe what was happening before my eyes," Boree Carlsson, 45, of Sweden, told Reuters from a hotel in the Thai resort of Phuket. "As I was standing there, a car actually floated into the lobby and overturned because the current was so strong."
+
+A tsunami -- the term is Japanese -- is a series of waves generated by underwater seismic disturbances, in this case the interface of the India and Burma tectonic plates. Seismologists with the United States Geological Survey said the ocean west of Sumatra and the island chains to its north was a hot zone for earthquakes because of a nonstop collision occurring there between the India plate, beneath the Indian Ocean seabed, and the Burma plate under the islands and that part of the continent.
+
+The India plate is moving at about two inches a year to the northeast, creating pressure that releases, sporadically, in seismic activity. But this was an especially devastating earthquake, the fourth most powerful in 100 years.
+
+Television images showed bodies floating in muddied waters. Cars went out to sea; boats came onto land. Snorkelers were dragged onto the beach, and sunbathers out to sea, Simon Clark, a photographer who was vacationing on Ngai Island in Thailand, told The Associated Press.
+
+Indonesia reported nearly 4,500 dead, most in the Banda Aceh area of Sumatra, a region that has been the site of a continuing civil war. In Sri Lanka, at least 6,000 were dead. In India, an estimated 2,300 died, with at least 1,700 confirmed dead in Tamil Nadu, the southern state that is home to this coastal city of Madras, officially known as Chennai.
+
+At least three Americans were reported killed, two in Sri Lanka and one in Thailand, according to Noel Clay, a State Department spokesman.
+
+The death toll is expected to climb, and world leaders, from President Bush to Pope John Paul II, expressed grief and called for aid for the victims.
+
+Many areas from the atolls of the Maldives to the Nicobar Islands of India were simply out of reach, with communication lines snapped. Thousands more people in those places are feared marooned or dead. India's home minister, Shivraj Patil, said there was no communication with 45,000 residents of the Nicobar Islands.
+
+On Marina Beach here in Madras, women selling fish and children playing cricket, morning walkers and tourists savoring the salt-scented air all died as the sea suddenly turned enemy. The water came with no warning, said S. Muttukumar, a fisherman.
+
+"We see the sea come forward," he said, describing how he ran and then swam from the 40-foot wave, grabbing onto catamarans for life support. "Everybody was running, but God saves little."
+
+In the lanes of his village across from Marina Beach, four middle-aged women, all fish sellers who had been unable to outrun the waves, lay embalmed in refrigerated glass cases, prepared for cremation on Tuesday.
+
+Here, Mr. Muttukumar said, the water had come up off the beach an extra 500 yards to the main road, lingered for about 10 minutes, then receded as if it had never been there at all, leaving the fishermen to fish for bodies. Early Monday morning, the beach was still littered with fishing boats and upended hand carts.
+
+At the decrepit Madras General Hospital, a board listed 38 dead. Twelve were unknown; the rest ranged in age from 4 to 87.
+
+The waves struck tourist resorts from Phuket in Thailand to Bentota in Sri Lanka at the peak of the tourist and holiday season, meaning that livelihoods will be devastated long after the count of lost lives is done.
+
+Whole fishing villages were washed away along coastlines, and thousands of fisherman who unknowingly put out to sea in the morning are missing. An Indian Air Force base on Car Nicobar Island was virtually washed away, according to television reports, and late reports suggested that at least 1,000 could be dead on the Nicobar Islands.
+
+The airport in Male, the capital of the Maldives, was closed, stranding foreign tourists overnight at the airport in Colombo, Sri Lanka's capital, after much of Male was submerged.
+
+The casualties spread across southern India: at least 200 dead in Andhra Pradesh state, 80 in Kerala, and 280 in Pondicherry.
+
+Sri Lanka, the island nation to India's south, was battered on both its east and west coasts. The Sri Lankan government declared a national disaster, and the Liberation Tigers of Tamil Eelam, the rebel group that controls swaths of northern and eastern Sri Lanka, said it would declare its own national emergency.
+
+The Tamilnet Web site cited reports from the Tamil Relief Organization that 1,000 bodies had been brought to hospitals in the north and east, with the toll expected to rise.
+
+Reuters reported looting in Sri Lanka, and officials said that at least 200 prisoners had escaped from a prison in Matara, about 100 miles south of Colombo, after it was damaged by the tsunami.
+
+In southern Thailand, more than 400 people were dead, according to news agencies. Among them were foreign tourists who had come to vacation at the area's popular beach resorts. Emergency workers rescued about 70 Thai and foreign divers from the famed Emeral Cave, and dozens were evacuated from around other islands, Reuters reported. Two Thais were killed at Emeral cave.
+
+India, reeling from its own casualties, found itself trying to assist its smaller neighbors. It sent five naval warships to Sri Lanka loaded with relief supplies and fielded an offer for similar help from the Maldives.
+
+The Indian government also issued a warning that residents should stay at least one kilometer, or six-tenths of a mile, away from the sea for the next two days for fear of aftershocks and more tsunamis.
+
+But even without further calamity, the devastation will take weeks to unfold and years to repair. Officials in some areas expressed concern that saline water could contaminate drinking water and ruin arable land. Hundreds of thousands of people are homeless, crowded into unsanitary temporary shelters, and bodies are likely to wash up for days.']
 Article_3=['Jagrati yaha article paste karien !!!!']
 
 Article_list=[Article_1,Article_2,Article_3]
